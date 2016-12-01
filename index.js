3fff15535b2103f2bc79b31398bfbb081254d43b
Bennett Staley <me@bennettstaley.com>
2016-12-01 12:15:35 -0600

    Initial index.js server file, added express and path to packages.json.

---
+++ index.js
@@ -0,0 +1,17 @@
+var express = require('express');
+var bodyParser = require('body-parser');
+var path = require('path');
+var app = express();
+module.exports = app;
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+
+app.get('/', function (req, res) {
+  res.sendFile(path.resolve(__dirname + '/index.html'));
+});
+
+var port = process.env.PORT || 4040;
+app.listen(port);
+console.log("Listening on port " + port);
