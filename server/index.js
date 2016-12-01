dc63f87cb24bd564ac4dbf58530fd862e294bfb1
Bennett Staley <me@bennettstaley.com>
2016-12-01 17:31:42 -0600

    changes have been made

--- server/index.js
+++ server/index.js
@@ -8,6 +8,7 @@ app.use(bodyParser.urlencoded({
     extended: true
 }));
 
+//changes have been made
 
 app.get('/', function (req, res) {
   res.sendFile(path.resolve(__dirname + '/../client/index.html'));
