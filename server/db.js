1ced4c7663df2654cf1f21cf79f7f0779c4f5a2e
Bennett Staley <me@bennettstaley.com>
2016-12-05 15:47:36 -0600

    Feedback = Comments internal recognition. its all comments now. + redirect to post project.

--- server/db.js
+++ server/db.js
@@ -15,9 +15,9 @@ module.exports = {
   postUser : postUser,
   getSessionById : getSessionById,
   postSession : postSession,
-  getFeedbackByProjectId : getFeedbackByProjectId,
-  getFeedbackByUserId : getFeedbackByUserId,
-  postFeedback : postFeedback
+  getCommentByProjectId : getCommentByProjectId,
+  getCommentByUserId : getCommentByUserId,
+  postComment : postComment
 };
 
 // ----- SETUP DB -----
@@ -141,12 +141,12 @@ function postSession(session, cb){
     });
 }
 
-// ----- FEEDBACK METHODS -----
+// ----- BACK METHODS -----
 
-function getFeedbackByProjectId(projectId, cb){
-  models.Feedback.find({projId: projectId})
+function getCommentByProjectId(projectId, cb){
+  models.Comment.find({projId: projectId})
     .then(function(res){
-      console.log('feedback for project' , res);
+      console.log('comment for project' , res);
       cb(null, res);
   })
     .catch(function(err){
@@ -155,10 +155,10 @@ function getFeedbackByProjectId(projectId, cb){
     });
 }
 
-function getFeedbackByUserId(userId, cb){
-  models.Feedback.find({userId: userId})
+function getCommentByUserId(userId, cb){
+  models.Comment.find({userId: userId})
     .then(function(res){
-      console.log('feedback for user' , res);
+      console.log('comment for user' , res);
       cb(null, res);
   })
     .catch(function(err){
@@ -167,8 +167,8 @@ function getFeedbackByUserId(userId, cb){
     });
 }
 
-function postFeedback(feedback, cb){
-  models.Feedback(feedback).save()
+function postComment(comment, cb){
+  models.Comment(comment).save()
     .then(function(res){
       cb(null, res);
     })
