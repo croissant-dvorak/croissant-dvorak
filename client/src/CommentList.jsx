11e69fb53266d5c64479eda426090c1209736bf7
Bob DuQuesnay <bobduquesnay@Bobs-MacBook-Air.local>
2016-12-05 15:20:21 -0600

    finsihed prelim code in CommentList.jsx and CommentListItem.jsx

--- client/src/CommentList.jsx
+++ client/src/CommentList.jsx
@@ -1,1 +1,14 @@
-CommentList.jsx
+//CommentList.jsx
+
+const React = require('react');
+const CommentListItem = require('./CommentListItem.jsx');
+
+const CommentList = props => (
+  <div>
+    {props.comments.map((comm, idx) => <CommentsListItem project={comm} key={idx} />)}
+  </div>
+);
+
+
+
+module.exports = CommentList;
