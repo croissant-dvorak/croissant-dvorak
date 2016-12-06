c9d021c1063d089fe3f2b92b9f682b0bc85d309f
Kevin <kjs2b@virginia.edu>
2016-12-05 19:07:16 -0600

    Comment area and comment input files added

---
+++ client/src/CommentArea.jsx
@@ -0,0 +1,13 @@
+const React = require('react');
+const CommentList = require('./CommentList.jsx');
+const CommentInput = require('./CommentInput.jsx');
+
+const CommentArea = props => (
+  <div>
+    
+  </div>
+);
+
+
+
+module.exports = CommentArea;
---
+++ client/src/CommentInput.jsx
@@ -0,0 +1,12 @@
+const React = require('react');
+
+
+const CommentInput = props => (
+  <div>
+    
+  </div>
+);
+
+
+
+module.exports = CommentInput;
